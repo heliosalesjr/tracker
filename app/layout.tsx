027ef00a834ac4cf931479e42a14049c1d5c5860
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-04 20:23:03 -0300

    log in with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
-
+import { ClerkProvider } from '@clerk/nextjs';
+import Header from '../components/Header';
 
 const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
@@ -16,14 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-  
+    <ClerkProvider>
       <html lang='en'>
         <body className={roboto.className}>
-          
-          <main className='container'>{children}</main>
+          <Header />
+          <main className='container'>
+            {children}
+          </main>
           
         </body>
       </html>
+    </ClerkProvider>
+  
+      
     
   );
 }
