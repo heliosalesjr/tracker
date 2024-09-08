5ccd4ae6dc1747723c0de1595877d6942e81a341
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-08 20:06:16 -0300

    guest user component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-
+import { checkUser } from '@/lib/checkUser';
 
 const Header = async () => {
 
-
+  const user = await checkUser();
+  
   return (
     <nav className='navbar'>
       <div className='navbar-container'>
