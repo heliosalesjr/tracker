c348f24dc9755c23f9ca4e61f6043f0c9339cbc8
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-10 14:47:56 -0300

    add transaction action

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,10 +1,19 @@
 'use client';
 
+import addTransaction from '../app/actions/addTransaction';
+
 const AddTransaction = () => {
   
 
   const clientAction = async (formData: FormData) => {
-    console.log(formData.get('text'), formData.get('amount'));
+    const { data, error } = await addTransaction(formData);
+
+    if (error) {
+        alert(error);
+      } else {
+        alert('Transaction added');
+        console.log(data);
+      }
 
   };
 
